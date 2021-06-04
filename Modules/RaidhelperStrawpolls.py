7521eff529274359bb0912cf6d5ea661d30b1796
Maik Stappers <m.stappers@posteo.net>
2021-06-04 17:03:06 +0200

    Raid helper strawpolls (#3)
    * Added module to evaluate Raidhelper Strawpolls
    
    * Added module to evaluate Raidhelper Strawpolls and show the result in Offi-chat

---
+++ Modules/RaidhelperStrawpolls.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import datetime
+import json
+
+
+def read_data_from_api(event_id):
+    result = {}
+    answers = {}
+    url = f'http://51.195.103.14:3000/api/raids/{event_id}'
+    auth_token = os.environ.get('RAIDHELPER_BEARER_TOKEN')
+    headers = {'authorization': f'Bearer {auth_token}'}
+    response = requests.get(url=url, headers=headers).json()
+    raw_date = response['raids']['date']
+    raw_time = response['raids']['time']
+    date = datetime.datetime.strptime(raw_date + '+' + raw_time, '%d-%m-%Y+%H:%M')
+    description = response['raids']['description']
+    question, answers_list = description.split('=')
+    for i, answer in enumerate(answers_list.split(',')):
+        answers.update({chr(65+i): {'text': answer,
+                                    'count': 0,
+                                    'voters': []
+                                    }})
+    for user in response['raidusers']:
+        vote = user['spec'][0]
+        answers[vote]['voters'].append(user['username'].replace('**', ''))
+        answers[vote]['count'] += 1
+    return question, answers, len(response['raidusers'])
+
+
+if __name__ == '__main__':
+    read_data_from_api(850125011661750282)
